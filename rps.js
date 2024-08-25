51b5235387658a53db9d233aeb3b638b00da08b6
Stuart Hahn <47613093+stuart-hahn@users.noreply.github.com>
2024-08-24 20:31:48 -0400

    feat: add ui, track scores, declare winner
    - clicking button plays a round of the game
    - win, loss, and tie all get recorded
    - when someone wins 5 times they are the game winner

--- rps.js
+++ rps.js
@@ -1,6 +1,7 @@
 const container = document.querySelector(".container")
 const buttons = document.querySelectorAll("button")
 const results = document.querySelector(".results")
+const scoreDiv = document.querySelector(".scores")
 // Available choices for the game
 const choices = ["rock", "paper", "scissors"];
 
@@ -22,32 +23,60 @@ let scores = {
   human: 0,
 };
 
+let winner = null
+
+let scoreDisplay = document.createElement("p")
+
 // Function to play a single round
 const playRound = (e) => {
   const computerChoice = getRandomChoice();
   const humanChoice = getHumanChoice(e);
-
+  
   if (computerChoice === humanChoice) {
-    console.log("It's a tie!");
+    const tieMessage = document.createElement("p")
+    tieMessage.textContent = "It's a tie."
+    results.appendChild(tieMessage)
   } else if (
     (computerChoice === "rock" && humanChoice === "scissors") ||
     (computerChoice === "paper" && humanChoice === "rock") ||
     (computerChoice === "scissors" && humanChoice === "paper")
   ) {
-    console.log(
-      `The computer wins because ${computerChoice} beats ${humanChoice}.`
-    );
+    const computerWinsMessage = document.createElement("p")
+    computerWinsMessage.textContent = `The computer wins because ${computerChoice} beats ${humanChoice}.`
+    results.appendChild(computerWinsMessage)
     scores.computer++;
   } else {
-    console.log(`You win because ${humanChoice} beats ${computerChoice}.`);
+    const humanWinsMessage = document.createElement("p")
+    humanWinsMessage.textContent = `You win because ${humanChoice} beats ${computerChoice}.`
+    results.appendChild(humanWinsMessage)
     scores.human++;
+  }
+
+  if (scores.computer === 5) {
+    winner = "The computer"
+    scoreDisplay.textContent = `The computer wins with a score of ${scores.computer} to ${scores.human}`
+    scoreDiv.appendChild(scoreDisplay)
+  } else if (scores.human === 5) {
+    winner = "You"
+    scoreDisplay.textContent = `You win with a score of ${scores.human} to ${scores.computer}`
+    scoreDiv.appendChild(scoreDisplay)
+  } else {
+    scoreDisplay.textContent = `Computer: ${scores.computer}, Human: ${scores.human}`
+    scoreDiv.appendChild(scoreDisplay)
   }
 };
 
 
-buttons.forEach(addEventListener("click", (e) => {
-  playRound(e)
-}))
+buttons.forEach(button => 
+  button.addEventListener("click", (e) => {
+    if (!winner) {
+      playRound(e)
+    } else {
+      scoreDisplay.textContent = `${winner} was the winner. Refresh to play again.`
+    }
+  })
+)
+
 
 
 // Function to play the game for 5 rounds and determine the overall winner
